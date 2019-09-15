4fddc5e816a89ca4c5f69a2ce615d74745cc5b7e
Andre Assef <andreassef02@gmail.com>
2019-09-14 21:42:00 -0400

    Proj JDBC para pc principal

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Departament;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departament obj = new Departament(1, "Books");
+		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Andre", "andre@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
+	}
+}
